ea63f5643ed44f1c4ff20ddf3831475f7a2030b5
Simranjit <simranjit12145@gmail.com>
2025-12-31 02:26:51 +0530

    New commit

--- web/chunk-AVMLATF7.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var t=[{path:"",redirectTo:"",pathMatch:"full"},{path:"",loadComponent:()=>import("./chunk-5K5DOGFS.js").then(o=>o.HomepageComponent)}];export{t as routes};
