5e63b799e4f92d2138378abf0e368c8699184eb0
Simranjit <simranjit12145@gmail.com>
2026-01-06 01:55:43 +0530

    New commit

--- web/browser/chunk-OGNX27PU.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var t=[{path:"",redirectTo:"",pathMatch:"full"},{path:"",loadComponent:()=>import("./chunk-CNKGXNEU.js").then(o=>o.HomepageComponent)}];export{t as routes};
