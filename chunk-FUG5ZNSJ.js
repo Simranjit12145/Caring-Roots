4339b5ea6bee6aa8e7fa73db94171ef709fc902e
Simranjit <simranjit12145@gmail.com>
2026-01-06 01:54:09 +0530

    Auto-generated commit

---
+++ chunk-FUG5ZNSJ.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var t=[{path:"",redirectTo:"",pathMatch:"full"},{path:"",loadComponent:()=>import("./chunk-O5QNY7RJ.js").then(o=>o.HomepageComponent)}];export{t as routes};
